2c5ef108998552779e404d77f79ffd7762397157
Rajesha Koppa Ramesha <49751425+RajeshRao93@users.noreply.github.com>
2022-06-22 21:25:07 +0200

    changing folder name

--- restaurant/app.js
+++ restaurant/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const restaurant = require('./src/restaurant');
+const restaurant = require('./repo/restaurant');
 const port = process.env.PORT || 6000
 app.use(express.json());
 app.use(bodyParser.json());
